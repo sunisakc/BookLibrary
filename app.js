d555b8416c54e4b16f464da39f2e06efd875c62e
sunisakc <sunisa.k@gmail.com>
2019-04-04 13:26:24 +0200

    Add Books to list

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 // UI Class: Handle UI Tasks
     class UI {
         static displayBooks(){
-            const StoredBook = [
+            const StoredBook =[
                 {
                     title: 'Book One',
                     author: 'Delia Owens',
@@ -20,7 +20,8 @@
                     title: 'Book One',
                     author: 'Eric Carle',
                     isbn:'98765004'
-                },{
+                },
+                {
                     title: 'Book One',
                     author: 'Werner Sonne',
                     isbn:'1234321567'
@@ -40,9 +41,17 @@
              <td>${book.title}</td>
              <td>${book.author}</td>
              <td>${book.isbn}</td>
-             <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+             <td><a href="#" class="btn btn-danger btn-sm 
+             delete">X</a></td>
             `;
+
             list.appendChild(row);
+        }
+
+        static clearFields() {
+            document.querySelector('#title').value = '';
+            document.querySelector('#author').value = '';
+            document.querySelector('#isbn').value = '';
         }
     }
 
@@ -64,6 +73,11 @@ document.querySelector('#book-form').addEventListener('submit',(e)=>
     //Instatiate book
     const book = new Book(title, author, isbn);
 
-    console.log(book)
+    // Add book to UI
+    UI.addBookToList(book);
+
+    //Method Clear fields
+    UI.clearFields();
+    
 });
 // Event: Remove A Book
